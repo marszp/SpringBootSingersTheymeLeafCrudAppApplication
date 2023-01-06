a920ceb101ed2a53c2017243205f5b3e211d8680
Stidz-PC <marszp8888@gmail.com>
2023-01-06 21:24:05 +0100

    Added simple JUnit test

---
+++ src/test/java/com/msz/SpringBootSingersTheymeLeafCrudApp/controller/SingerControlerTest.java
@@ -0,0 +1,20 @@
+package com.msz.SpringBootSingersTheymeLeafCrudApp.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.ui.Model;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+class SingerControlerTest {
+
+    @Test
+    void getHello() {
+
+        //Simple test to check that Junit works
+        assertEquals("helloworld","helloworld");
+
+    }
+}
